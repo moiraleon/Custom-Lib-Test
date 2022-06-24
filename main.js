507aa2993c1cf40f59077db616b9964ca906a51a
Moira Leon <86265787+moiraleon@users.noreply.github.com>
2022-06-24 13:29:20 -0600

    fixing link

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import{add, greet} from ".https://raw.githubusercontent.com/moiraleon/Custom-Lib-Test/main/util.js";
+import{add, greet} from "https://raw.githubusercontent.com/moiraleon/Custom-Lib-Test/main/util.js";
 
 console.log(add([1,2,3]))
 console.log(greet("Mary"))
