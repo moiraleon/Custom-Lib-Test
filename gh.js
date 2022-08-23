f3ac795becd8428ded62869245f6c89c770fdf7b
Moira Leon <86265787+moiraleon@users.noreply.github.com>
2022-08-23 12:05:02 -0600

    updates

---
+++ gh.js
@@ -0,0 +1,5 @@
+var myModule = require('https://raw.githubusercontent.com/moiraleon/Custom-Lib-Test/main/module.js');
+
+var myModuleInstance = new myModule();
+console.log(myModuleInstance.hello());
+console.log(myModuleInstance.goodbye());
